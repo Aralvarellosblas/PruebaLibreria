42a538d3fc83e609d2a1a1ce946116cf72f005f2
artur <artur@ArturoMSI>
2018-01-25 09:20:32 +0100

    Añadido metodo de la librería

--- src/añadirlibrerias/AñadirLibrerias.java
+++ src/añadirlibrerias/AñadirLibrerias.java
@@ -1,5 +1,6 @@
 package añadirlibrerias;
 
+import com.otros.milibreria.MisMetodos;
 /**
  *
  * @author Arturo
@@ -10,7 +11,7 @@ public class AñadirLibrerias {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        
+        System.out.println(MisMetodos.unMetodo(77));
     }
 
 }
