e737fd8fa6aabf97c1e1b09aebee94b41ced720a
artur <artur@ArturoMSI>
2018-01-26 09:02:42 +0100

    Utilizados metodos de la nueva librería

--- src/añadirlibrerias/AñadirLibrerias.java
+++ src/añadirlibrerias/AñadirLibrerias.java
@@ -1,17 +1,20 @@
 package añadirlibrerias;
 
 import com.otros.milibreria.MisMetodos;
+import com.juan.dialogos.Devolver;
+import com.juan.dialogos.Introducir;
 /**
  *
  * @author Arturo
  */
-public class AñadirLibrerias {
+public class AñadirLibrerias{
 
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) {
+    public static void main(String[] args){
         System.out.println(MisMetodos.unMetodo(77));
+        Devolver.mostrarFloat(7.7f);
+        Devolver.mostrarInt(8);
     }
-
 }
